a94c2a292b6d030f914747717496235375add6ab
Alexandr Dorovskikh <alexandr.dorovskikh@gmail.com>
2022-11-16 22:11:19 +0700

    task15 added

---
+++ Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿bool WeekendDay(int day)
+{
+    if (day >= 6) {return true;} else {return false;}    
+}
+
+Console.Write("Enter the number of the day of the week: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (WeekendDay(num)) 
+{
+    Console.Write("It's a weekend");
+}
+else
+{
+    Console.Write("It's a working day");
+}
