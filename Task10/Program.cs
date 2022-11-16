f24933c56507a87a8d8ad51f401f0b1b665d64ed
Alexandr Dorovskikh <alexandr.dorovskikh@gmail.com>
2022-11-16 21:33:56 +0700

    task10 added

---
+++ Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿int ThirdDigit(int num)
+{
+    return (num / 10) % 10;
+}
+
+Console.Write("Enter three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Third digit is: ");
+Console.WriteLine(ThirdDigit(num));
